492591edcc95abb72c49eb727b87e054fc746165
Serchcode <pe280591@uaeh.edu.mx>
2017-07-06 21:52:06 -0500

    anchors done

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,13 +6,13 @@ class Footer extends Component{
 		return(
 			<div>
 			<div className="futer">
-				<i className="fa fa-github fa-3x ico" aria-hidden="true"></i>
-				<i className="fa fa-linkedin fa-3x ico" aria-hidden="true"></i>
-				<i className="fa fa-globe fa-3x ico" aria-hidden="true"></i>
+				<a className="ancla" href="https://github.com/Serchcode"><i className="fa fa-github fa-3x ico" aria-hidden="true"></i></a>
+				<a className="ancla" href="https://www.linkedin.com/in/sergio-p%C3%A9rez-bautista-794492114/"><i className="fa fa-linkedin fa-3x ico" aria-hidden="true"></i></a>
+				<a className="ancla" href="http://www.serchlab.com"><i className="fa fa-globe fa-3x ico" aria-hidden="true"></i></a>
 				<br/>
 			</div>
 			<div className="Hack">
-			<p>Made by Serchlab.com 2017</p>
+			<a href="http://www.serchlab.com"><p>Made by Serchlab.com 2017</p></a>
 			</div>
 			</div>
 			);
