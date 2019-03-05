c3c4254981ccf914eae122c7cc0e583193ecabcb
Serchcode <pe280591@uaeh.edu.mx>
2019-03-05 14:00:59 -0600

    links and date

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -8,11 +8,11 @@ class Footer extends Component{
 			<div className="futer">
 				<a className="ancla" href="https://github.com/Serchcode"><i className="fa fa-github fa-3x ico" aria-hidden="true"></i></a>
 				<a className="ancla" href="https://www.linkedin.com/in/sergio-p%C3%A9rez-bautista-794492114/"><i className="fa fa-linkedin fa-3x ico" aria-hidden="true"></i></a>
-				<a className="ancla" href="http://www.serchlab.com"><i className="fa fa-globe fa-3x ico" aria-hidden="true"></i></a>
+				<a className="ancla" href="https://sergioperez.herokuapp.com/"><i className="fa fa-globe fa-3x ico" aria-hidden="true"></i></a>
 				<br/>
 			</div>
 			<div className="Hack">
-			<a href="http://www.serchlab.com"><p>Made by Serchlab.com 2017</p></a>
+			<a href="https://sergioperez.herokuapp.com/"><p>Made by Serchlab.com 2019</p></a>
 			</div>
 			</div>
 			);
